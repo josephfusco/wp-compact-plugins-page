996188920f6edf0e1f50b0b39f273deae62eddbb
Joseph Fusco <hello@josephfus.co>
2016-04-05 17:46:43 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# WP Compact Plugins Page
+
+[![License](https://img.shields.io/badge/license-GPL--2.0%2B-green.svg)](http://www.gnu.org/licenses/gpl-2.0.html)
+
+View long lists of plugins with ease on the wp-admin plugins page.
---
+++ wp-compact-plugins-page.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name:    WP Compact Plugins Page
+Plugin URI:     http://github.com/josephfusco/wp-compact-plugins-page/
+Description:    View long lists of plugins with ease on the wp-admin plugins page.
+Version:        1.0.0
+Author:         Joseph Fusco
+Author URI:     http://josephfus.co/
+License:        GPLv2 or later
+License URI:    http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function wpcpp_styles() {
+    wp_enqueue_style( 'acfv_theme_transparent', plugins_url( '/compact.css' , __FILE__ ) );
+}
+add_action( 'admin_enqueue_scripts', 'wpcpp_styles' );
